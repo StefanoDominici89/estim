e9b5824f3f148e81a0ebb815630918d245ccf74b
StefanoDominici89 <30604545+StefanoDominici89@users.noreply.github.com>
2021-06-25 10:13:10 +0200

    Update license and readme (#1)

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,20 @@
-//! Collection of lightweight statistical estimators accessible through the shared Estimator trait.
-//! 
+//! Collection of lightweight statistical estimators implementing a common interface through the Estimator trait.
+//!
+//! # Examples
+//!
+//! ```
+//! let samples: std::Vec<f64> = vec![0.0, 1.0, 2.0, 3.0, 4.0];
+//! let mut mse = estimators::MeanSquaredError::<f64>::new();
+//! let mut avg = estimators::Average::<f64>::new();
+//!
+//! for sample in &samples{
+//!     mse.update(sample);
+//!     avg.update(sample);
+//! }
+//!
+//! println!("AVG: {}",avg.evaluate());
+//! println!("AVG and MSE: ({},{})",mse.average.evaluate(),mse.evaluate());
+//! ```
 mod estimator_lib;
 mod estimator_trait;
 
